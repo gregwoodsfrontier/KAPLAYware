28f71e2f386250047c777a08f981bd8aefed359b
Richard Chung <iwannaplaygamesjp@gmail.com>
2025-02-25 20:25:47 +0000

    untest: added checked Area to game.ts and created assets folder.

--- games/ricjones/transform/game.ts
+++ games/ricjones/transform/game.ts
@@ -1,24 +1,29 @@
 import { assets } from "@kaplayjs/crew";
 import { Minigame } from "../../../src/kaplayware";
+import { KAPLAYCtx } from "kaplay";
 
 const newGame: Minigame = {
-	prompt: "transform",
-	author: "ricjones",
-	hue: 1,
-	urlPrefix: "games/ricjones/assets",
-	load(ctx) {
-		ctx.loadSprite("bean", assets.bean.sprite);
-	},
-	start(ctx) {
-		const game = ctx.make([ctx.timer()]);
+  prompt: "transform",
+  author: "ricjones",
+  hue: 1,
+  urlPrefix: "games/ricjones/assets",
+  load(ctx) {
+    ctx.loadSprite("bean", assets.bean.sprite);
+  },
+  start(ctx: KAPLAYCtx) {
+    const game = ctx.make([ctx.timer()]);
 
-		const bean = game.add([
-			ctx.sprite("bean"),
-			ctx.pos(),
-		]);
+    const bean = game.add([ctx.sprite("bean"), ctx.pos(100, 100)]);
 
-		return game;
-	},
+    const checkArea = game.add([
+      ctx.rect(200, 100),
+      ctx.pos(ctx.width() * 0.5, ctx.height() * 0.45),
+      ctx.outline(5),
+      ctx.color(255, 0, 0),
+    ]);
+
+    return game;
+  },
 };
 
 export default newGame;
--- src/main.ts
+++ src/main.ts
@@ -2,43 +2,37 @@ import getGame from "../games/amyspark-ng/get/game";
 import spamGame from "../games/amyspark-ng/spam/game";
 import chaseGame from "../games/nanopoison/chase/game";
 import kaplayware from "./kaplayware";
+import transformGame from "../games/ricjones/transform/game";
 
 const games = [
-	getGame,
-	spamGame,
-	chaseGame,
+  //getGame,
+  //spamGame,
+  //chaseGame,
+  transformGame,
 ];
 
 const ware = kaplayware(games);
 const k = ware.kCtx;
 
 k.scene("focus", () => {
-	k.setBackground(k.BLACK);
+  k.setBackground(k.BLACK);
 
-	k.add([
-		k.text("CLICK TO FOCUS"),
-		k.pos(k.center()),
-		k.anchor("center"),
-	]);
+  k.add([k.text("CLICK TO FOCUS"), k.pos(k.center()), k.anchor("center")]);
 
-	k.onClick(() => k.go("game"));
+  k.onClick(() => k.go("game"));
 });
 
 k.scene("game", () => {
-	ware.nextGame();
+  ware.nextGame();
 });
 
 k.scene("gameover", () => {
-	k.debug.log(":(");
+  k.debug.log(":(");
 
-	k.add([
-		k.text("you lost :("),
-		k.pos(k.center()),
-		k.anchor("center"),
-	]);
+  k.add([k.text("you lost :("), k.pos(k.center()), k.anchor("center")]);
 });
 
 k.onLoad(() => {
-	if (k.isFocused()) k.go("game");
-	else k.go("focus");
+  if (k.isFocused()) k.go("game");
+  else k.go("focus");
 });
